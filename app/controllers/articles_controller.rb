942a083d3439d97dc574ebcededbf5a529088394
heewon <heewon221@naver.com>
2018-05-01 11:14:18 +0000

    first commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,5 +17,20 @@ class ArticlesController < ApplicationController
             redirect_to @article
         end
         
-
+        def destroy
+            article = Article.find(params[:id])
+            article.destroy
+            
+            redirect_to articles_path
+        end
+        def edit
+            @article = Article.find(params[:id])
+        end
+        def update
+            params.permit!
+            @article = Article.find(params[:id])
+            @article.update(params[:article])
+            redirect_to @article
+        end
+        
 end
